dd206a3a6d394b639f475b8a1724cad0264e7c7a
hyojining <gywls1390@gmail.com>
2023-09-06 12:49:57 +0900

    helloweb #2

--- helloweb/src/main/java/servlets/HelloServlet.java
+++ helloweb/src/main/java/servlets/HelloServlet.java
@@ -15,7 +15,8 @@ public class HelloServlet extends HttpServlet {
 		response.setContentType("text/html; charset=utf-8");
 		PrintWriter pw = response.getWriter();
 		
-		pw.print("<h1>Hello World</h1>");
+		String name = request.getParameter("name");
+		pw.print("Hello " + name);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
